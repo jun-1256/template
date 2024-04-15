e68232bdcc13ff8078c6fbe6fdaeac9c17de136c
jun <jun@localhost>
2024-04-15 22:28:53 +0900

    2024/04/15

--- src/com/diworksdev/template/action/BuyItemConfirmAction.java
+++ src/com/diworksdev/template/action/BuyItemConfirmAction.java
@@ -1,4 +1,6 @@
-package com.diworksdev.template.action; import java.sql.SQLException;
+package com.diworksdev.template.action;
+
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
@@ -7,6 +9,7 @@ import com.diworksdev.template.dao.BuyItemCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BuyItemConfirmAction extends ActionSupport implements SessionAware {
+
 	private Map<String,Object> session;
 	public String execute() throws SQLException {
 		BuyItemCompleteDAO buyItemCompleteDAO = new BuyItemCompleteDAO();
